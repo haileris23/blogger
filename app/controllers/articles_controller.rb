07918b18ec5c73f58b2c971a8d3de3409facc2bb
haileris23 <blandc@goldmail.etsu.edu>
2013-04-13 14:27:58 -0400

    Now with editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,38 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id]) 
   end
+
   def new
     @article = Article.new 
   end
+
+  def create
+    @article = Article.new(params[:article])
+    @article.save 
+    redirect_to article_path(@article) 
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy 
+    redirect_to articles_path 
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id]) 
+    @article.update_attributes(params[:article]) 
+    flash.notice = "Article '#{@article.title}' updated! Good jerb!"
+    redirect_to article_path(@article) 
+  end
+
 end
